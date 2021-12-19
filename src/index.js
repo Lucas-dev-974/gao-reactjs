fa2cad74ebf4e1197a58a86364f3ac06913b1e22
lucas <lucas.lvn97439@gmail.com>
2021-12-19 19:52:18 +0400

    theme applied to app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import App from './router'
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from 'styled-components'
 
+const theme = {
+  colors: {
+    dark: '',
+    green: '',
+  },
 
+  fonts: {
+    body: 'Arial, sans-serif'
+  }
+}
 
 ReactDOM.render(
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider> 
     </BrowserRouter>,
   document.getElementById("app")
 );
