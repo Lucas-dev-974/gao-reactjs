94b5ce933ed7c3c77bb8fb8e506d949f8a5c4764
lucas <lucas.lvn97439@gmail.com>
2021-12-19 19:55:03 +0400

    Assignation list render function updated, remove assignation added

--- src/components/Computer.js
+++ src/components/Computer.js
@@ -1,10 +1,11 @@
 import React from "react";
-import axios from "axios";
 
-import { Card, CardHeader, CardContent, Typography, List, ListItem, IconButton, ListItemText, Icon } from '@mui/material';
-import { green, red } from '@mui/material/colors';
+import { Card, CardHeader, CardContent, ListItem, IconButton, ListItemText, Icon } from '@mui/material';
+import { red } from '@mui/material/colors';
 
-import AddComputer from './AddComputer'
+import AddAttribution from './AddAttribution'
+import ConfirmDelete from "./ConfirmDelete"
+import ApiService from '../Services/ApiService'
 
 export default class Computer extends React.Component{
     constructor(props) {
@@ -13,20 +14,24 @@ export default class Computer extends React.Component{
         this.state = {
             attributions: [],
             computer: this.props.computer,
+            date: this.props.date,
             horraires: [],
             listView: null
         }
 
         this.buildAttributions = this.buildAttributions.bind(this)
         this.buildHours        = this.buildHours.bind(this)
+        this.pushAttribution   = this.pushAttribution.bind(this)
+        this.removeAttribitution = this.removeAttribitution.bind(this)
     }
     
     componentDidMount(){
         this.buildAttributions()
     }
 
-    buildAttributions(){
-        this.state.computer.attributions.forEach(attribution => {
+    async buildAttributions(){
+        console.log('in build attributions: ', this.state.computer.attributions);
+        this.state.computer.attributions.forEach(async attribution => {
             if(!isNaN(attribution.horraire)){
                 let horraire = parseInt(attribution.horraire)
                 this.state.attributions[horraire] = {
@@ -35,70 +40,105 @@ export default class Computer extends React.Component{
                 }
             }
         })
+        // console.log(this.state.attributions);
         this.buildHours()
     }
 
-    ItemListView(horraire){
-        var ligne;
-        if(horraire.attribution !== false){
-            ligne = <ListItemText >{horraire.index + 'H'} {horraire.attribution.client}</ListItemText>  
-        }else{
-            ligne = <ListItemText primary={horraire.index}  />
-        }
-
-        return ligne
+    pushAttribution(attribution){
+        this.setState(prevState => ({
+            computer:{
+                ...prevState.computer,
+                attributions: [...this.state.computer.attributions, attribution]
+            } 
+        }))
+        this.buildAttributions()
     }
 
-    ItemListIcon(horraire){
-        var icons
-        if(horraire.attribution == false){
-            // icons = <IconButton edge="end" aria-label="delete"><Icon  sx={{ color: green[500] }}>add_circle</Icon></IconButton>
-            icons = <AddComputer ></AddComputer>
-        }else{
-            icons = <IconButton edge="end" aria-label="delete"><Icon sx={{ color: red[500] }}>remove_circle</Icon></IconButton>
-        }
+    removeAttribitution(attribution){
+        console.log(this.state.computer.attributions);
+        ApiService.delete('/api/attributions/' + attribution.id)
+        .then( async (data) => {
+            
 
-        return icons
+            new Promise((resolve, reject) => {
+                let attributions = this.state.attributions.filter(attr => {
+                    if(attribution.id != attr.id) return attr
+                })
+
+                resolve(attributions)
+            }).then((data) => {
+                console.log('in promise');
+                console.log(data);
+                // this.setState(prevState => ({
+                //     computer: {
+                //         ...prevState.computer,
+                //         attributions: data
+                //     }
+                // }))
+
+                this.setState({
+                    attributions: data
+                })
+                this.buildAttributions()
+            })
+        })
     }
 
-    buildListView(){
-        var horraires = <Typography>No data</Typography>
+    async buildHours(){
+        console.log('in build houres');
+        console.log(this.state.attributions)
+        let horraires = []
+        for(let i = 0; i < 11; i++){
+            horraires.push({
+                index: i + 8,
+                attribution: (typeof this.state.attributions[i + 8] !== 'undefined') ? this.state.attributions[i + 8] : false
+            })
+        }
+        await this.setState({ horraires: horraires })
+        this.renderAttributionsList()
+    }
+
+    // -----------  RENDER FUNCTIONS -------------
+
+    renderAttributionsList(){
+        let horraires = null
+        console.log('in render list');
+        console.log(this.state.horraires);
         if(this.state.horraires.length > 0){
             horraires = this.state.horraires.map((horraire) => {
                 return (
-                <ListItem  key={horraire.index} secondaryAction={this.ItemListIcon(horraire)}>
-                    {this.ItemListView(horraire)}
-                   {/* {if(horraire.attribution != false){<ListItemText primary={horraire.index} secondary={horraire.client} />}} */}
-                    
-                </ListItem>
-
+                    <ListItem  key={horraire.index} secondaryAction={this.ItemListIcon(horraire)}>
+                        {this.ItemListView(horraire)}
+                    </ListItem>
                 )
             })
         }
-
-        this.state.listView = horraires
+        
+        this.setState({ listView: horraires })
         this.forceUpdate()
     }
 
-    buildHours(){
-        for(let i = 0; i < 11; i++){
-            this.state.horraires.push({
-                index: i + 8,
-                attribution: (typeof this.state.attributions[i + 8] != 'undefined') ? this.state.attributions[i + 8] : false
-            })
-        }
-        this.buildListView()
-        console.log(this.state.horraires);
+    ItemListView(horraire){
+        if(horraire.attribution !== false) return <ListItemText >{horraire.index + 'H'} {horraire.attribution.client}</ListItemText>  
+        else return <ListItemText primary={horraire.index}  />
+    }
+
+    ItemListIcon(horraire){
+        if(horraire.attribution == false) return <AddAttribution computer={this.state.computer} date={this.state.date} horraire={horraire} pushAttribution={this.pushAttribution}></AddAttribution>
+        else return <IconButton edge="end" onClick={() => this.removeAttribitution(horraire.attribution)} aria-label="delete"><Icon sx={{ color: red[500] }}>remove</Icon></IconButton>    
     }
 
     render(){
         return(
-            <Card sx={{ maxWidth: 345 }} key={this.state.computer.id}>
-                <CardHeader title={this.state.computer.name}/>
+            <Card  sx={{minWidth: '300px'}} md={{minWidth: '400px'}} key={this.state.computer.id}>
+                <CardHeader className="bg-dark" title={this.state.computer.name} 
+                    action={
+                        <ConfirmDelete url='/api/computers/'  id={this.props.computer.id} removeComputer={this.props.removeComputer} />
+                    }
+                />
+
                 <CardContent>
-                    <List key={this.state.lisKey}>
-                        {this.state.listView}
-                    </List>
+                    {this.state.listView}
                 </CardContent>
             </Card>
         )
