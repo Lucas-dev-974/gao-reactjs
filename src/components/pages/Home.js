90c9874707b096292c8a36079b0d51804f0e2d65
lucas <lucas.lvn97439@gmail.com>
2021-12-19 19:58:27 +0400

    render of computer list function added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,77 +1,104 @@
 import React from "react";
-import axios from "axios";
-
 import Grid from '@mui/material/Grid';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-
+import TextField from '@mui/material/TextField'
 import Computer from '../Computer';
-
+import ApiService from '../../Services/ApiService'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { Icon, Stack } from "@mui/material";
+import Header from '../AppBar'
+import { DatePicker } from "@mui/lab";
+import { Container } from "@mui/material";
+import AddComputer from '../AddComputer'
 export default class Home extends React.Component{
     constructor(props) {
         super(props);
-  
         this.state = {
-            computers: [
-                {id: 1, name: 'Ordinateur 1', attributions: [
-                    {id: 1, horraire: 8, client: {id: 1, name: 'test'}}
-                ]},
-                {id: 2, name: 'Ordinateur 1', attributions: [
-                    {id: 1, horraire: 9, client: {id: 1, name: 'test'}}
-                ]},
-                {id: 3, name: 'Ordinateur 1', attributions: [
-                    {id: 1, horraire: 10, client: {id: 1, name: 'test'}}
-                ]},
-            ],
-
-            date: ''
+            computers: [],
+            date: new Date().toISOString().substr(0, 10),
         }
+
+        this.renderComputersList = this.renderComputersList.bind(this)
+        this.getComputers      = this.getComputers.bind(this)
+        this.handleDateChange  = this.handleDateChange.bind(this)
+        this.pushComputer      = this.pushComputer.bind(this)
+        this.removeComputer    = this.removeComputer.bind(this)
     }
     
     componentDidMount(){
-        this.isLoggedIn()
+        this.isUserLogged()
+        this.getComputers()
     }
 
-    initComputers(){
-        // console.log(this.state.computers);
-        var computers = this.state.computers.map(function(computer){
-            return <Grid key={computer.id} item xs={4}>  <Computer computer={computer}></Computer> </Grid>
+    getComputers(){
+        ApiService.get('/api/computers/' + this.state.date)
+        .then(({data}) => {
+            this.setState({ computers: data.computers })
+        }).catch(error => {
+            console.log(error.response);
+        })
+    }
+
+    pushComputer(computer){
+        this.setState({
+            computers: [...this.state.computers, computer]
+        })
+    }
+
+    removeComputer(id){
+   
+        this.setState({ computers: this.state.computers.filter(computer => computer.id !== id) })
+    }
+
+    renderComputersList(){
+        let date = this.state.date
+        let removeComputer = this.removeComputer
+        let computers = this.state.computers.map(function(computer) {
+            return <Grid key={computer.id} item >  <Computer computer={computer} date={date} removeComputer={removeComputer}></Computer> </Grid>
         })
         return computers
     }
 
-    isLoggedIn(){
+    isUserLogged(){
         if(!window.localStorage.getItem('AccessToken'))  window.location.href = '/login'
         else{
-            axios.get('/api/auth/test-token')
-            .then(({data}) => {
-                if(!data.success) window.location.href = '/login'
-                else{
-
-                }
+            ApiService.get('/api/auth/')
+            .catch(error => { 
+                window.location.href = '/login' 
             })
         }
     }
 
+    handleDateChange(date){
+        this.setState({ date: date, computers: [] })
+        this.getComputers()
+    }
+
     render(){
         return(
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <AppBar position="static">
-                        <Toolbar variant="dense">
-                            <Typography variant="h6" component="div"  style={{ align: "center" }}>
-                                Scroll to Hide App Bar
-                            </Typography>
-                        </Toolbar>
-                    </AppBar>                    
-                </Grid>
-                <Grid item xs={10}>
-                    <Grid container spacing={2}>
-                        { this.initComputers() }
+            <div>
+                <Header />
+                <div  className="home-container">
+                    <Grid container spacing={4}>
+                        <Grid item xs={5} md={2}>
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <DatePicker label="Date" value={this.state.date} onChange={this.handleDateChange} 
+                                renderInput={(params) => <TextField  variant="standard" {...params} />} />
+                            </LocalizationProvider>
+                        </Grid>
+                        <Grid item xs={1} style={{display: 'flex', marginTop: '15px'}} >
+                            <Icon style={{color: '#09151E', paddingTop: '10px', marginLeft: '10px'}}>computer</Icon>
+                            <AddComputer pushComputer={this.pushComputer} />
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Grid>
+
+                    <Grid container spacing={2}  justifyContent='center' direction="row" style={{marginTop: '20px'}}>
+                        { this.renderComputersList() }
+                    </Grid>
+
+                </div>
+
+            </div>
         )
     }
 }
